b73d255b8480c24ea8407fca0db9bd3a7577c03f
Liying Shi <lynns1009@gmail.com>
2021-09-04 14:00:54 +1000

    init

---
+++ src/main/java/com/example/afterpay/AfterPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.afterpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/afterpay/AfterPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.afterpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
